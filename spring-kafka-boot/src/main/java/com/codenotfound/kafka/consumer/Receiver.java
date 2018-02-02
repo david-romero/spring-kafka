d98b822ff5090342af88683334e17600849020b4
David Romero Alcaide <david.romero4.contractor@bbva.com>
2018-02-02 14:44:45 +0100

    Firt version of tutorial

--- spring-kafka-boot/src/main/java/com/codenotfound/kafka/consumer/Receiver.java
+++ spring-kafka-boot/src/main/java/com/codenotfound/kafka/consumer/Receiver.java
@@ -1,27 +1,43 @@
 package com.codenotfound.kafka.consumer;
 
-import java.util.concurrent.CountDownLatch;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.codenotfound.kafka.model.Event;
+import com.codenotfound.kafka.repositories.EventRepository;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Component
+@Slf4j
 public class Receiver {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
-
-  private CountDownLatch latch = new CountDownLatch(1);
-
-  public CountDownLatch getLatch() {
-    return latch;
-  }
-
-  @KafkaListener(topics = "${kafka.topic.boot}")
-  public void receive(ConsumerRecord<?, ?> consumerRecord) {
-    LOGGER.info("received payload='{}'", consumerRecord.toString());
-    latch.countDown();
-  }
+	@Autowired
+	private EventRepository eventRepository;
+	
+	@KafkaListener(topics = "myTopic", containerFactory = "kafkaListenerContainerFactory" , errorHandler = "kafkaListenerErrorHandler")
+	public void receive(@Payload String message,
+			@Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition,
+			@Header(KafkaHeaders.RECEIVED_TOPIC) String topic, @Header("kafka_receivedTimestamp") long ts,
+			Acknowledgment ack) {
+		log.info("received payload=[{}] , from partition:[{}] topic:[{}] , timestamp:[{}]", message, partition, topic, ts);
+		final Event event = new Event();
+		final LocalDateTime date = LocalDateTime.ofInstant(Instant.ofEpochMilli(ts), ZoneId.systemDefault());
+		event.setDate(date);
+		event.setInsertDate(LocalDateTime.now());
+		event.setPayload(message);
+		final Event persistedEvent = eventRepository.save(event);
+		log.info("persisted event: {}", persistedEvent);
+		ack.acknowledge();
+		log.info("ack sent payload=[{}] , from partition:[{}] topic:[{}] , timestamp:[{}]", message, partition, topic, ts);
+	}
 }
